7ef71f50f9bede2a790581b54fa4276656c98704
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2024-05-23 13:57:03 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUICustomEventsTask
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace MAUICustomEventsTask
+{
+    // Write code below this line
+
+    // Write code above this line
+
+    public class apiEventArgs : EventArgs
+    {
+        public string Link { get; }
+
+        public apiEventArgs(string content)
+        {
+            this.Link = content;
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public apiEvent apiService = new apiEvent();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            getImageButton.Clicked += GetImage;
+            apiService.OnResponse += SetImage;
+        }
+
+        private void SetImage(object? sender, apiEventArgs e)
+        {
+            mainImage.Source = e.Link;
+        }
+
+        private void GetImage(object? sender, EventArgs e)
+        {
+            string apiUrl = "https://random.imagecdn.app/v1/image";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = response.Content.ReadAsStringAsync().Result;
+                    apiService.OnApiResponse(content);
+                }
+            }
+        }
+    }
+}
